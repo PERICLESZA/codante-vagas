6f66103e039175bcdae8f3d2dfecd7b6cc9325a5
PERICLESZA <pericles1011@yahoo.com.br>
2025-09-25 12:04:39 -0300

    tratando a tabela de vagas ainda hardcode

--- app/vagas/page.tsx
+++ app/vagas/page.tsx
@@ -1,7 +1,21 @@
+import JobItem from "@/components/cards/job-item";
+
 export default function Vaga() {
   return (
-    <div>
-      <h1>Vagas</h1>
-    </div>
+    <main className="py-10">
+      <h2 className="font-display mb-12 text-2xl font-bold">Todas as Vagas</h2>
+      <div className="space-y-8">
+        <JobItem />
+        <JobItem />
+        <JobItem />
+        <JobItem />
+        <JobItem />
+        <JobItem />
+        <JobItem />
+        <JobItem />
+        <JobItem />
+        <JobItem />
+      </div>
+    </main>
   );
 }
